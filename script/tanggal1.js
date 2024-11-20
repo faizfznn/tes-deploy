f11abeb65e657f2e05756a1e36e4979a2c5283a7
Faiz Fauzan <faiz150605@gmail.com>
2024-11-20 11:10:29 +0700

    frontend-fix

--- script/tanggal1.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("lanjutkanButton").addEventListener("click", function() {
-    // Arahkan ke halaman verifikasi.html
-    window.location.href = "verifikasi.html";
-});
--- script/verifikasi.js
+++ script/verifikasi.js
@@ -12,3 +12,21 @@ document.getElementById("verifikasiForm").addEventListener("submit", function(ev
         window.location.href = "index.html"; // Redirect ke halaman home
     }, 1000); // 3 detik durasi loading
 });
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    const inputs = form.querySelectorAll("input");
+    let valid = true;
+    inputs.forEach((input) => {
+        if (!input.value) {
+            valid = false;
+            input.style.border = "1px solid red";
+        } else {
+            input.style.border = "";
+        }
+    });
+    if (!valid) {
+        event.preventDefault();
+        alert("Harap isi semua kolom!");
+    }
+});
